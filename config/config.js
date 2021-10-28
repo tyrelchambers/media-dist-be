80c1105d6dbfa9f049a0f30563911846c971a09c
Tyrel Chambers <tychambers3@gmail.com>
2021-10-28 18:39:59 -0400

    .

---
+++ config/config.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+module.exports = {
+  development: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: "media_db",
+    dialect: "postgres",
+  },
+  test: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: "socialite_test_db",
+    dialect: "postgres",
+  },
+  production: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DB,
+    host: "media_db",
+    dialect: "postgres",
+  },
+};
